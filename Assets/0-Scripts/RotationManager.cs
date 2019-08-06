0572cb98f6ebf40f99d14d7fda02124632158b0b
Murat Yakin <yakin_murat@yahoo.com>
2019-08-07 00:59:28 +0300

    working on 3 rotations and blow

--- Assets/0-Scripts/RotationManager.cs
+++ Assets/0-Scripts/RotationManager.cs
@@ -12,8 +12,8 @@ public class RotationManager : MonoBehaviour {
     public bool isRotatingClockwise;
     public bool isAnimating;
 
-
     private Transform outlinerOldParent;
+
 
     public void SetObjects(Transform anOriginalObj, Transform aWing1Obj, Transform aWing2Obj) {
         originalObj = anOriginalObj;
@@ -23,18 +23,18 @@ public class RotationManager : MonoBehaviour {
 
     public void RotateOnce(Transform anOutliner, bool isClockwise) {
         if (!isAnimating) {
+            int rotationCount = 0;
             ParentObjs();
             outlinerOldParent = anOutliner;
             Transform objectToRotate = GameObject.FindGameObjectWithTag("OutlinerBody").transform;
             anOutliner.SetParent(objectToRotate);
             isRotatingClockwise = isClockwise;
-            StartCoroutine(AnimateRotation(objectToRotate));
+            StartCoroutine(AnimateRotation(objectToRotate, rotationCount));
         }
     }
 
 
-
-    private IEnumerator AnimateRotation(Transform anOutlinerBody) {
+    private IEnumerator AnimateRotation(Transform anOutlinerBody, int aRotationCount) {
         Quaternion targetRotation = transform.rotation * Quaternion.Euler(0, 0, 120f);
         Quaternion targetOutlinerRotation = anOutlinerBody.rotation * Quaternion.Euler(0, 0, 120f);
         if (!isRotatingClockwise) {
@@ -43,7 +43,7 @@ public class RotationManager : MonoBehaviour {
         }
 
         float timer = 0;
-        float animtionDuration = 1f;
+        float animtionDuration = 0.5f;
         while (timer<animtionDuration) {
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, timer / animtionDuration);
             anOutlinerBody.rotation = Quaternion.Slerp(anOutlinerBody.rotation, targetOutlinerRotation, timer / animtionDuration);
@@ -51,22 +51,21 @@ public class RotationManager : MonoBehaviour {
             yield return null;
         }
 
-        StopCoroutine(AnimateRotation(anOutlinerBody));
-
         RotateObjectGrids();
         UpdateGameControllerDictionary();
         UpdateHexNeighbors();
         originalObj.GetComponent<HexObject>().UpdateAngleActiveIndex(isRotatingClockwise);
-        UnParentObjs();
-        InputManager.im.isRotationOn = false;
-        //GameController.gc.DumpOutlinedObjects();
 
-        isAnimating = false;
-
-
-
-        anOutlinerBody.parent.SetParent(outlinerOldParent);
-
+        if (CheckForBlowingObjects() || aRotationCount==2) { // end rotation
+            StopCoroutine(AnimateRotation(anOutlinerBody, aRotationCount));
+            UnParentObjs();
+            InputManager.im.isRotationOn = false;
+            isAnimating = false;
+            anOutlinerBody.parent.SetParent(outlinerOldParent);
+        } else {
+            aRotationCount++;
+            yield return AnimateRotation(anOutlinerBody, aRotationCount);
+        }
     }
 
     private void RotateObjectGrids() {
@@ -107,6 +106,55 @@ public class RotationManager : MonoBehaviour {
         wing1Obj.SetParent(transform);
         wing2Obj.SetParent(transform);
     }
+
+    private bool CheckForBlowingObjects() {
+        bool somethingBlew = false;
+
+        Transform[] originalBlow = ShouldBlow(originalObj);
+        if (originalBlow[0]!=null) {
+            somethingBlew = true;
+        }
+
+        Transform[] wing1Blow = ShouldBlow(wing1Obj);
+        if (wing1Blow[0] != null) {
+            somethingBlew = true;
+        }
+
+        Transform[] wing2Blow = ShouldBlow(wing2Obj);
+        if (wing2Blow[0] != null) {
+            somethingBlew = true;
+        }
+        return somethingBlew;
+    }
+    private Transform[] ShouldBlow(Transform aReferenceObject) {
+        string[] names = new string[6];
+        names[0] = aReferenceObject.GetComponent<HexObject>().neighbor30.gameObject.name.Substring(0, 6);
+        names[1] = aReferenceObject.GetComponent<HexObject>().neighbor90.gameObject.name.Substring(0, 6);
+        names[2] = aReferenceObject.GetComponent<HexObject>().neighbor120.gameObject.name.Substring(0, 6);
+        names[3] = aReferenceObject.GetComponent<HexObject>().neighbor210.gameObject.name.Substring(0, 6);
+        names[4] = aReferenceObject.GetComponent<HexObject>().neighbor270.gameObject.name.Substring(0, 6);
+        names[5] = aReferenceObject.GetComponent<HexObject>().neighbor330.gameObject.name.Substring(0, 6);
+
+        string thisName = transform.gameObject.name.Substring(0, 6);
+
+        for (int i = 0; i < 6; i++) {
+            if (thisName == names[0] && thisName == names[1]) {
+                return new Transform[] { aReferenceObject.GetComponent<HexObject>().neighbor30, aReferenceObject.GetComponent<HexObject>().neighbor90 };
+            } else if (thisName == names[1] && thisName == names[2]) {
+                return new Transform[] { aReferenceObject.GetComponent<HexObject>().neighbor90, aReferenceObject.GetComponent<HexObject>().neighbor120 };
+            } else if (thisName == names[2] && thisName == names[3]) {
+                return new Transform[] { aReferenceObject.GetComponent<HexObject>().neighbor120, aReferenceObject.GetComponent<HexObject>().neighbor210 };
+            } else if (thisName == names[3] && thisName == names[4]) {
+                return new Transform[] { aReferenceObject.GetComponent<HexObject>().neighbor210, aReferenceObject.GetComponent<HexObject>().neighbor270 };
+            } else if (thisName == names[4] && thisName == names[5]) {
+                return new Transform[] { aReferenceObject.GetComponent<HexObject>().neighbor270, aReferenceObject.GetComponent<HexObject>().neighbor330 };
+            } else if (thisName == names[5] && thisName == names[0]) {
+                return new Transform[] { aReferenceObject.GetComponent<HexObject>().neighbor330, aReferenceObject.GetComponent<HexObject>().neighbor30 };
+            }
+        }
+        return new Transform[] {null,null};
+    }
+
     private void UnParentObjs() {
         originalObj.SetParent(originalObj.GetComponent<HexObject>().parentingGrid);
         wing1Obj.SetParent(wing1Obj.GetComponent<HexObject>().parentingGrid);
