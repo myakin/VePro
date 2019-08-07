968ea8658a3cc38a508f056c4f5f1e179651ffa3
Murat Yakin <yakin_murat@yahoo.com>
2019-08-07 22:19:27 +0300

    reverted back and now continuing

--- Assets/0-Scripts/GridManager.cs
+++ Assets/0-Scripts/GridManager.cs
@@ -5,32 +5,5 @@ using UnityEngine;
 public class GridManager : MonoBehaviour {
 
     public Vector2 id;
-    public bool isBlown;
-
-    public void MoveColumn() {
-        Vector2 upperGridIndex = new Vector2(id.x, id.y + 1);
-        Transform targetHex = null;
-        if (upperGridIndex.y < GameController.gc.gridSize.y) {
-            if (GameController.gc.gridsAndContents[upperGridIndex].grid.transform.childCount>0) {
-                if (GameController.gc.gridsAndContents[upperGridIndex].grid.transform.GetChild(0).GetComponent<HexObject>().isBlown) {
-                    upperGridIndex = new Vector2(id.x, id.y + 2);
-                    if (upperGridIndex.y < GameController.gc.gridSize.y) {
-                        if (GameController.gc.gridsAndContents[upperGridIndex].grid.transform.childCount > 0) {
-                            if (!GameController.gc.gridsAndContents[upperGridIndex].grid.transform.GetChild(0).GetComponent<HexObject>().isBlown) {
-                                targetHex = GameController.gc.gridsAndContents[upperGridIndex].grid.transform.GetChild(0);
-                            }
-                        }
-                    }
-                } else {
-                    targetHex = GameController.gc.gridsAndContents[upperGridIndex].grid.transform.GetChild(0);
-                }
-            }
-        }
-
-        if (targetHex!=null) {
-            targetHex.GetComponent<HexObject>().SetFallTargetGrid(transform);
-        }
-    }
-
 
 }
